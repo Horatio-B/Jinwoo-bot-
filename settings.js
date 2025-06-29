9b04be1756712ab02d696ff0ad1c28f6843b884a
Horatio-B <botolovehoratio+1@gmail.com>
2025-06-29 11:13:51 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -17,7 +17,7 @@ if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 //--------------------[ SESSION ID ]----------------------//
 
-global.SESSION_ID = process.env.SESSION_ID || 'jinwoo:~ojNHhTxD#jAUOUoBaHQEr4_N1i3LYE8RNOU5vpfAHKFTfP4xoGaI' 
+global.SESSION_ID = process.env.SESSION_ID || 'jinwoo:~0iUXRLxJ#8pemMvPyYCk7QuLTBFiFZcMeI8KeIH-OjaGxw8gaDg8' 
 //Enter your Jinwoo session id here; must start with jinwoo:~
 
 //--------------------[ BOT NAME ]----------------------//
